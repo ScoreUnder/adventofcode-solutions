5b361157f5c63f1ac610554ad08c196dd61367a1
Score_Under <seejay.11@gmail.com>
2018-12-13 00:29:50 +0000

    Use String instead of Vector[Char] for plant field
    This simplifies the code a little (subjectively speaking).

--- 2018/12/notlife.scala
+++ 2018/12/notlife.scala
@@ -4,29 +4,29 @@ val RULE_REGEX = "([.#]{5}) => ([.#])".r
 val (initialState, rules) = {
   val instructions = scala.io.Source.fromFile("input").getLines.toVector
 
-  (instructions.collect { case INITIAL_STATE_REGEX(state) => state.toVector }.ensuring(_.size == 1).head,
-   instructions.collect { case RULE_REGEX(left, right) => left.toVector -> right.ensuring(_.size == 1).head }.toMap[Seq[Char],Char] withDefaultValue '.')
+  (instructions.collect { case INITIAL_STATE_REGEX(state) => state }.ensuring(_.size == 1).head,
+   instructions.collect { case RULE_REGEX(left, right) => left -> right.ensuring(_.size == 1).head }.toMap withDefaultValue '.')
 }
 
-implicit class RichVector[T](val me: Vector[T]) {
-  def dropRightWhile(f: T => Boolean): Vector[T] = me take (me.lastIndexWhere(!f(_)) + 1)
+implicit class RichString(val me: String) {
+  def dropRightWhile(f: Char => Boolean): String = me take (me.lastIndexWhere(!f(_)) + 1)
 }
 
-val PADDING = Vector.fill(4)('.')  // 2 to allow growth up to 2 tiles left/right, plus 2 to pad out the sliding window
-case class SimState(offset: Int, plants: Vector[Char]) {
+val PADDING = "." * 4  // 2 to allow growth up to 2 tiles left/right, plus 2 to pad out the sliding window
+case class SimState(offset: Int, plants: String) {
   def padded = copy(offset = offset + PADDING.size, plants = PADDING ++ plants ++ PADDING)
   def trimmed = copy(offset = offset - plants.indexOf('#'), plants = plants.dropWhile(_ == '.').dropRightWhile(_ == '.'))
 }
 
 val simStream = Stream.iterate(SimState(offset = 0, plants = initialState).trimmed) { state =>
   val newState = state.padded
-  val newPlants = newState.plants.sliding(5).map(rules(_)).toVector
+  val newPlants = newState.plants.sliding(5).map(rules(_)).mkString
   newState.copy(offset = newState.offset - 2, plants = newPlants).trimmed
 }
 
 def printState(state: SimState) = {
   print(" " * (3 - state.offset))
-  println(state.plants.mkString)
+  println(state.plants)
 }
 
 def sumPotIndices(state: SimState) = {
