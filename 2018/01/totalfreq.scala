467445551bf89d506ff9613fee1bf3e291644f59
Score_Under <seejay.11@gmail.com>
2018-12-15 17:27:54 +0000

    Golf solution 01 a bit

--- 2018/01/totalfreq.scala
+++ 2018/01/totalfreq.scala
@@ -1,10 +1,6 @@
 val freqs = scala.io.Source.fromFile("freqs.txt").getLines.map(_.toLong).toVector
 println(s"Total freq: ${freqs.sum}")
 val seen = new scala.collection.mutable.HashSet[Long]
-val infiniteFreqs: Stream[Long] = freqs.toStream #::: infiniteFreqs
-val firstRepeat = infiniteFreqs.scanLeft(0L)(_+_).find { x =>
-  val stop = seen contains x
-  seen += x
-  stop
-}
+def infiniteFreqs = Iterator.continually(freqs).flatten
+val firstRepeat = infiniteFreqs.scanLeft(0L)(_+_).find { x => !seen.add(x) }
 println(s"First repeat element: $firstRepeat")
