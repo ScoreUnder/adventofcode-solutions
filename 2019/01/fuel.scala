09f7cf0294a78eb03a57bdb0551886e98617ef9e
Score_Under <seejay.11@gmail.com>
2019-12-13 13:11:57 +0000

    Solution 2019-01 part 2

--- 2019/01/fuel.scala
+++ 2019/01/fuel.scala
@@ -1,3 +1,16 @@
 val masses = scala.io.Source.fromFile("input").getLines.map(_.toInt).toSeq
 
-println(masses.map(_/3 - 2).sum)
+def massToFuel(mass: Int) = 0 max (mass/3 - 2)
+println(masses.map(massToFuel).sum)
+
+def cascadeFuel(initial: Int) = {
+  @annotation.tailrec
+  def inner(mass: Int, acc: Int): Int = {
+    massToFuel(mass) match {
+      case 0 => acc
+      case fuel => inner(fuel, fuel + acc)
+    }
+  }
+  inner(initial, 0)
+}
+println(masses.map(cascadeFuel).sum)
