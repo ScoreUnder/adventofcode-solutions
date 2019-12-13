0a3307020133ce7428c001f5179c8aa22c7a0963
Score_Under <seejay.11@gmail.com>
2019-12-13 13:37:02 +0000

    Solution 2019-02 part 2

--- 2019/02/intcode.py
+++ 2019/02/intcode.py
@@ -17,21 +17,30 @@ def execute(program, pc):
     return pc
 
 
-def execute_loop(program):
+def execute_loop(program, args):
+    program = program[:]
+    program[1:3] = args
+
     pc = 0  # Program Counter
     while pc is not None:
         pc = execute(program, pc)
+
+    return program[0]
 
 
 def main():
     program = open("input").read().replace('\n', '').split(',')
     program = [int(x) for x in program]
 
-    # Weird problem spec but okay:
-    program[1:3] = [12, 2]
-    execute_loop(program)
+    result = execute_loop(program, [12, 2])
+    print(result)
 
-    print(program[0])
+    for noun in range(0, len(program)):
+        for verb in range(0, len(program)):
+            result = execute_loop(program, [noun, verb])
+            if result == 19690720:
+                print(noun * 100 + verb)
+                break
 
 
 main()
