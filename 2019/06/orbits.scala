4cb2efc6917bf136328ee87e515cf67353e0fd86
Score_Under <seejay.11@gmail.com>
2019-12-16 13:25:52 +0000

    Complete 2019-06 part 2

--- 2019/06/orbits.scala
+++ 2019/06/orbits.scala
@@ -19,3 +19,15 @@ val getOrbitCount: String => Int = memoized { planet: String =>
 
 // Part 1
 println(orbits.keys.view.map(getOrbitCount).sum)
+
+def getTransferNum(from: String, to: String) = {
+  @annotation.tailrec
+  def gatherTransfers(from: String, to: String, acc: Int): Int =
+    if (from == to) acc
+    else if (getOrbitCount(from) > getOrbitCount(to)) gatherTransfers(orbits(from), to, acc + 1)
+    else gatherTransfers(from, orbits(to), acc + 1)
+  gatherTransfers(orbits(from), orbits(to), 0)
+}
+
+// Part 2
+println(getTransferNum("YOU", "SAN"))
