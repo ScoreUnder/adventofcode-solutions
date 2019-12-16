d2c3957ad8fdac0a3e1b5595a10e67e5beea446f
Score_Under <seejay.11@gmail.com>
2019-12-16 16:45:24 +0000

    2019-09 Part 1

--- 2019/09/intcode.scala
+++ 2019/09/intcode.scala
@@ -5,54 +5,76 @@ import scala.util.chaining._
 import scala.jdk.CollectionConverters._
 import java.util.concurrent.Executors
 
-class IntcodeInterpreter(origProgram: Seq[Int]) {
-  var pc = 0
+type ProgInt = BigInt
+def ProgInt(value: String): ProgInt = BigInt(value)
+
+implicit class RichInt(val me: Int) {
+  def pow(other: Int) = math.pow(me, other).toInt
+}
+
+class IntcodeInterpreter(origProgram: Seq[ProgInt]) {
+  var pc = 0  // Program counter
+  var dp = 0  // Data pointer
   var program = origProgram.toBuffer
-  val input = new LinkedBlockingQueue[Int]
-  val output = new LinkedBlockingQueue[Int]
 
   sealed trait Opcode {
     def run(modes: Int): Unit
   }
-  private case class UnitOpcode(args: Int, func: Seq[Int] => Unit) extends Opcode {
+  private case class UnitOpcode(args: Int, func: Seq[ProgInt] => Unit) extends Opcode {
     def run(modes: Int) = func(consumePtrs(args, modes))
   }
-  private case class WritebackOpcode(args: Int, func: Seq[Int] => Int) extends Opcode {
-    def run(modes: Int) = writePtr(func(consumePtrs(args, modes)))
+  private case class WritebackOpcode(args: Int, func: Seq[ProgInt] => ProgInt) extends Opcode {
+    def run(modes: Int) = writePtr(func(consumePtrs(args, modes)), modes / 10.pow(args))
   }
 
   val opcodes = Map[Int, Opcode](
     1 -> WritebackOpcode(2, _.sum),
     2 -> WritebackOpcode(2, _.product),
-    3 -> WritebackOpcode(0, { _ => input.take() }),
-    4 -> UnitOpcode(1, { v => output.add(v(0)) }),
-    5 -> UnitOpcode(2, { p => if (p(0) != 0) pc = p(1) }),
-    6 -> UnitOpcode(2, { p => if (p(0) == 0) pc = p(1) }),
+    3 -> WritebackOpcode(0, { _ => io.StdIn.readLine("Need input: ").toInt}),
+    4 -> UnitOpcode(1, { p => println(p(0)) }),
+    5 -> UnitOpcode(2, { p => if (p(0) != 0) pc = p(1).toInt }),
+    6 -> UnitOpcode(2, { p => if (p(0) == 0) pc = p(1).toInt }),
     7 -> WritebackOpcode(2, { p => if (p(0) < p(1)) 1 else 0 }),
     8 -> WritebackOpcode(2, { p => if (p(0) == p(1)) 1 else 0 }),
+    9 -> UnitOpcode(1, { p => dp += p(0).toInt }),
     99 -> UnitOpcode(0, { _ => pc = -1 }),
   )
 
-  def consume(count: Int): Seq[Int] = {
-    val res = program.slice(pc, pc + count)
+  def consume(count: Int): Seq[ProgInt] = {
+    val res = (pc until (pc + count)).map(readFrom)
     pc += count
     res.toSeq
   }
 
-  def consumePtrs(count: Int, modes: Int): Seq[Int] = {
-    def alterPtrs(ptrs: Seq[Int], modes: Int): List[Int] =
-      if (ptrs.isEmpty) Nil
-      else if (modes % 10 == 1) ptrs.head :: alterPtrs(ptrs.tail, modes / 10)
-      else program(ptrs.head) :: alterPtrs(ptrs.tail, modes / 10)
+  def consumePtrs(count: Int, modes: Int): Seq[ProgInt] = {
+    def alterPtrs(ptrs: Seq[ProgInt], modes: Int): List[ProgInt] =
+      ptrs match {
+        case Seq() => Nil
+        case ptr +: others => (modes % 10 match {
+            case 0 => readFrom(ptr.toInt)
+            case 1 => ptr
+            case 2 => readFrom(dp + ptr.toInt)
+          }) :: alterPtrs(others, modes / 10)
+      }
     alterPtrs(consume(count), modes)
   }
 
-  def writePtr(value: Int): Unit = program(consume(1)(0)) = value
+  def writePtr(value: ProgInt, mode: Int): Unit = {
+    val Seq(positionBig) = consume(1)
+    val position = positionBig.toInt + (if (mode % 10 == 2) dp else 0)
+    if (position >= program.size) {
+      program ++= Iterator.fill(position - program.size + 1)(0)
+    }
+    program(position) = value
+  }
+
+  def readFrom(ptr: Int): ProgInt =
+    program.applyOrElse(ptr, { _: Int => 0 })
 
   def step(): Unit = {
     val (modes, opcode) = consume(1)(0) /% 100
-    opcodes.getOrElse(opcode, throw new Exception(s"Bad opcode $opcode"))
-      .run(modes)
+    opcodes.getOrElse(opcode.toInt, throw new Exception(s"Bad opcode $opcode"))
+      .run(modes.toInt)
   }
 
   def run(): Unit = while (running) step()
@@ -62,50 +84,13 @@ class IntcodeInterpreter(origProgram: Seq[Int]) {
 
 def main(): Unit = {
   val program = io.Source.fromFile("input").getLines
-    .flatMap(_.split(",")).map(_.toInt).toSeq
+    .flatMap(_.split(",")).map(ProgInt(_)).toSeq
 
   def part1 = {
-    val inputs = (0 to 4).permutations
-    val results = for (input <- inputs)
-      yield input -> input.foldLeft(0) { (acc, inp) =>
-        new IntcodeInterpreter(program).tap { interp =>
-          interp.input.addAll(List(inp, acc).asJava)
-          interp.run()
-        }.output.take()
-      }
-    results.maxBy(_._2)
-  }
-
-  def part2 = {
-    val inputs = (5 to 9).permutations
-    val threadPool = Executors.newCachedThreadPool()
-    val results = for (input <- inputs)
-      yield {
-        val machines = (for (digit <- input)
-          yield new IntcodeInterpreter(program).tap { interp =>
-            interp.input.add(digit)
-          }).toSeq
-        val machinesRunning = (for (machine <- machines)
-          yield threadPool.submit({ () => machine.run() }: Runnable)).toSeq
-        val pipes = (for ((out, in) <- machines zip (machines.tail :+ machines.head))
-          yield threadPool.submit({ () => while (true) { in.input.add(out.output.take()) } }: Runnable)).toSeq
-        machines(0).input.add(0)
-        while (machines(0).running) {
-          // slightly-less-busy wait
-          machines(0).input.add(machines(0).input.take())
-        }
-        val result = machines(0).input.take()
-        machinesRunning.foreach { _.cancel(true) }
-        pipes.foreach { _.cancel(true) }
-        input -> result
-      }
-    val result = results.maxBy(_._2)
-    threadPool.shutdownNow()
-    result
+    new IntcodeInterpreter(program).run()
   }
 
   println(part1)
-  println(part2)
 }
 
 main()
