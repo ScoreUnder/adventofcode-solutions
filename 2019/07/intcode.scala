5b205525a33ef0068b9fe3515097ae8f6f078f21
Score_Under <seejay.11@gmail.com>
2019-12-16 14:12:56 +0000

    Complete 2019-07 part 2 (wow I hate it)

--- 2019/07/intcode.scala
+++ 2019/07/intcode.scala
@@ -1,13 +1,15 @@
 #!/usr/bin/env scala
-import collection.mutable
+import java.util.concurrent.LinkedBlockingQueue
 import Integral.Implicits._
 import scala.util.chaining._
+import scala.jdk.CollectionConverters._
+import java.util.concurrent.Executors
 
 class IntcodeInterpreter(origProgram: Seq[Int]) {
   var pc = 0
   var program = origProgram.toBuffer
-  val input = mutable.Queue.empty[Int]
-  val output = mutable.Queue.empty[Int]
+  val input = new LinkedBlockingQueue[Int]
+  val output = new LinkedBlockingQueue[Int]
 
   sealed trait Opcode {
     def run(modes: Int): Unit
@@ -22,8 +24,8 @@ class IntcodeInterpreter(origProgram: Seq[Int]) {
   val opcodes = Map[Int, Opcode](
     1 -> WritebackOpcode(2, _.sum),
     2 -> WritebackOpcode(2, _.product),
-    3 -> WritebackOpcode(0, { _ => input.dequeue() }),
-    4 -> UnitOpcode(1, { v => output ++= v }),
+    3 -> WritebackOpcode(0, { _ => input.take() }),
+    4 -> UnitOpcode(1, { v => output.add(v(0)) }),
     5 -> UnitOpcode(2, { p => if (p(0) != 0) pc = p(1) }),
     6 -> UnitOpcode(2, { p => if (p(0) == 0) pc = p(1) }),
     7 -> WritebackOpcode(2, { p => if (p(0) < p(1)) 1 else 0 }),
@@ -53,21 +55,57 @@ class IntcodeInterpreter(origProgram: Seq[Int]) {
       .run(modes)
   }
 
-  def run(): Unit = while (pc >= 0) step()
+  def run(): Unit = while (running) step()
+
+  def running = pc >= 0
 }
 
 def main(): Unit = {
   val program = io.Source.fromFile("input").getLines
     .flatMap(_.split(",")).map(_.toInt).toSeq
-  val inputs = (0 to 4).permutations
-  val results = for (input <- inputs)
-    yield input -> input.foldLeft(0) { (acc, inp) =>
-      new IntcodeInterpreter(program).tap { interp =>
-        interp.input ++= inp :: acc :: Nil
-        interp.run()
-      }.output.dequeue()
-    }
-  println(results.maxBy(_._2))
+
+  def part1 = {
+    val inputs = (0 to 4).permutations
+    val results = for (input <- inputs)
+      yield input -> input.foldLeft(0) { (acc, inp) =>
+        new IntcodeInterpreter(program).tap { interp =>
+          interp.input.addAll(List(inp, acc).asJava)
+          interp.run()
+        }.output.take()
+      }
+    results.maxBy(_._2)
+  }
+
+  def part2 = {
+    val inputs = (5 to 9).permutations
+    val threadPool = Executors.newCachedThreadPool()
+    val results = for (input <- inputs)
+      yield {
+        val machines = (for (digit <- input)
+          yield new IntcodeInterpreter(program).tap { interp =>
+            interp.input.add(digit)
+          }).toSeq
+        val machinesRunning = (for (machine <- machines)
+          yield threadPool.submit({ () => machine.run() }: Runnable)).toSeq
+        val pipes = (for ((out, in) <- machines zip (machines.tail :+ machines.head))
+          yield threadPool.submit({ () => while (true) { in.input.add(out.output.take()) } }: Runnable)).toSeq
+        machines(0).input.add(0)
+        while (machines(0).running) {
+          // slightly-less-busy wait
+          machines(0).input.add(machines(0).input.take())
+        }
+        val result = machines(0).input.take()
+        machinesRunning.foreach { _.cancel(true) }
+        pipes.foreach { _.cancel(true) }
+        input -> result
+      }
+    val result = results.maxBy(_._2)
+    threadPool.shutdownNow()
+    result
+  }
+
+  println(part1)
+  println(part2)
 }
 
 main()
