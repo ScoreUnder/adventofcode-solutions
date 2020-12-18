41446d1395ebf29d8106ecac34617c26d64d7085
Score_Under <seejay.11@gmail.com>
2020-12-18 15:23:58 +0000

    ocamlformat

--- 2020/06/plane.ml
+++ 2020/06/plane.ml
@@ -1,43 +1,43 @@
 open Batteries
-
-module CharSet = Set.Make(Char)
+module CharSet = Set.Make (Char)
 
 let charset_of_string s =
-    let rec aux i set =
-        if i < 0 then set else aux (i - 1) (CharSet.add s.[i] set) in
-    aux (String.length s - 1) CharSet.empty
+  let rec aux i set =
+    if i < 0 then set else aux (i - 1) (CharSet.add s.[i] set)
+  in
+  aux (String.length s - 1) CharSet.empty
 
 let questions =
-    File.lines_of "input"
-    |> Enum.fold (fun lines v ->
-                    if v = ""
-                    then [] :: lines
-                    else
-                      let [@warning "-8"] group :: rest = lines in
-                      let ngroup = v :: group in
-                        ngroup :: rest)
-      [[]]
+  File.lines_of "input"
+  |> Enum.fold
+       (fun lines v ->
+         if v = "" then [] :: lines
+         else
+           let[@warning "-8"] (group :: rest) = lines in
+           let ngroup = v :: group in
+           ngroup :: rest)
+       [ [] ]
 
-let sum = List.fold_left (+) 0
+let sum = List.fold_left ( + ) 0
+
 let reduce f = function
   | x :: xs -> List.fold_left f x xs
   | [] -> raise (Invalid_argument "empty list passed to reduce")
 
 let sum_assenting_values joiner =
-    questions
-    |> List.map (fun group ->
-        group
-        |> List.map charset_of_string
-        |> reduce joiner
-        |> CharSet.cardinal)
-    |> sum
+  questions
+  |> List.map (fun group ->
+         group |> List.map charset_of_string |> reduce joiner
+         |> CharSet.cardinal)
+  |> sum
 
 let part1 = sum_assenting_values CharSet.union
+
 let part2 = sum_assenting_values CharSet.inter
 
 let main =
-    ignore (Printf.printf "%d\n" part1);
-    ignore (Printf.printf "%d\n" part2)
+  ignore (Printf.printf "%d\n" part1);
+  ignore (Printf.printf "%d\n" part2)
 
 (*let main =
     let print_lst = List.iter (Printf.printf "%s\n") in
