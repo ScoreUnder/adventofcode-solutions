7516aeab77314382675722e9ff6818dbfcfccc27
Score_Under <seejay.11@gmail.com>
2020-12-23 06:17:44 +0000

    day 12 part 2

--- 2020/12/navigation.ml
+++ 2020/12/navigation.ml
@@ -2,7 +2,11 @@ open Batteries
 open Printf
 
 let navi_instructions = File.lines_of "input" |> map (fun line ->
-                                                        line.[0], int_of_string (String.lchop line)
+                                                        let instr = line.[0] and mag = int_of_string (String.lchop line) in
+                                                          (match instr with
+                                                            |'L'|'R' -> assert ((mag mod 90) = 0)
+                                                            | _-> ());
+                                                          instr, mag
 ) |> List.of_enum
 
 let (+%) x y = (* positive modulo *)
@@ -26,7 +30,32 @@ let rec navigate x y dir = function
   | [] -> x, y
   | _ -> assert false
 
+let coord_rotate x y times =
+  let times = times +% 4 in
+    match times with
+      | 0 -> x, y
+      | 1 -> -y, x
+      | 2 -> -x, -y
+      | 3 -> y, -x
+      | _ -> assert false
+
+let rec navigate_p2 x y (wx, wy) = function
+  | ('N', d) :: tl -> navigate_p2 x y (wx, (wy - d)) tl
+  | ('S', d) :: tl -> navigate_p2 x y (wx, (wy + d)) tl
+  | ('E', d) :: tl -> navigate_p2 x y ((wx + d), wy) tl
+  | ('W', d) :: tl -> navigate_p2 x y ((wx - d), wy) tl
+  | ('L', d) :: tl -> navigate_p2 x y (coord_rotate wx wy (-d / 90)) tl
+  | ('R', d) :: tl -> navigate_p2 x y (coord_rotate wx wy (d / 90)) tl
+  | ('F', d) :: tl -> navigate_p2 (x + d * wx) (y + d * wy) (wx, wy) tl
+  | [] -> x, y
+  | _ -> assert false
+
 let part1 () =
   manhattan_abs (navigate 0 0 0 navi_instructions)
 
-let () = printf "Part 1: %d\n%!" (part1 ())
+let part2 () =
+  manhattan_abs (navigate_p2 0 0 (10, -1) navi_instructions)
+
+let () =
+  printf "Part 1: %d\n%!" (part1 ());
+  printf "Part 2: %d\n%!" (part2 ())
